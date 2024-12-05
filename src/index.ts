5f747f10db44562e7eafb0a631470a99b98fc605
Sachin Pathak <pathaks411@gmail.com>
2024-12-05 08:34:01 +0530

    bullBoard configuration done

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 
-import express,{Express} from "express";
+import express, { Express } from "express";
 
+import bullBoardUi from "./config/bullBoardConfig";
 import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
 import SampleWorker from "./workers/sampleWorker";
 
 
-const app:Express = express();
+const app: Express = express();
 
 
-app.use("/api",apiRouter);
+app.use("/api", apiRouter);
+app.use("/ui", bullBoardUi.getRouter());
 
-app.listen(serverConfig.PORT,()=>{
-    console.log("Sachin Pathak:",serverConfig.PORT);
+app.listen(serverConfig.PORT, () => {
+    console.log("Sachin Pathak:", serverConfig.PORT);
+    console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
     SampleWorker("SampleQueue");
-    sampleQueueProducer("SampleJob",{
+    sampleQueueProducer("SampleJob", {
         name: "Sachin",
         company: "Corewave",
         position: "Nodejs Developer",
